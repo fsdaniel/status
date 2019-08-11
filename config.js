ff1e10f55644b680222edde9b7737dc3dbfe33f4
Daniel <daniel.koopmans@gmail.com>
2019-08-12 00:52:52 +0200

    trying it out

--- config.js
+++ config.js
@@ -1,5 +1,35 @@
 module.exports = {
-  title: 'System Status',
-  name: 'system_status',
-  description: 'Netlify - Static Generated Example'
-};
+  "title": "PalaceWorld status",
+  "name": "palaceworld_status",
+  "description": "Status of services provided by palaceworld",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  content: {
+    frontMatterFormat: 'yaml',
+    systems: [
+      'gamebots',
+      'petbots',
+      'members.palaceworld.net',
+      'www.palaceworld.net',
+      'api.palaceworld.net',
+      'proprooms v2'
+    ]
+  },
+  theme: {
+    scheduled: {
+      position: 'aboveGlobalStatus'
+    }
+  },
+  head: {
+    link: [
+      { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3e4e88' }
+    ]
+  }
+}
---
+++ content/2019-08-11_new-status-page-test.md
@@ -0,0 +1,16 @@
+---
+title: new status page test
+date: '2019-08-11T22:52:06.895Z'
+severity: under-maintenance
+affectedsystems:
+  - gamebots
+  - petbots
+  - members.palaceworld.net
+  - www.palaceworld.net
+  - api.palaceworld.net
+  - proprooms v2
+resolved: false
+---
+Trying out the new status page system
+
+<!--- language code: en -->
